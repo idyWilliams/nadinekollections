4fcac4c51db6414326c49dc0bac3bc7d171c601b
idyWilliams <widorenyin0@gmail.com>
2025-12-27 01:10:58 +0100

    fixxes

--- components/admin/ProductForm.tsx
+++ components/admin/ProductForm.tsx
@@ -22,9 +22,6 @@ import {
   DropdownMenu,
   DropdownMenuCheckboxItem,
   DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { ProductCard } from "@/components/customer/ProductCard";
